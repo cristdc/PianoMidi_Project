25d499e400cbd93c47e2da2bc34bf28ba6318333
rmornun12 <roberto.m.n2005@gmail.com>
2024-05-09 14:37:01 +0200

    Commit Medible-2

--- pianito/src/teclas/Medible.java
+++ pianito/src/teclas/Medible.java
@@ -1,10 +1,6 @@
 package teclas;
 
 public interface Medible extends ElementoVisual{
-    public default int getAnchura(){
-        throw new UnsupportedOperationException();
-    }
-    public default int getAltura(){
-        throw new UnsupportedOperationException();
-    }
+    public int getAnchura();
+    public int getAltura();
 }
