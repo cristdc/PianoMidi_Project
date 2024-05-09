9076a220b68dbb5ca9f68dad35e7bab30ecf05ed
osopardo895 <alexcesarfer1234@gmail.com>
2024-05-09 13:38:46 +0200

    new class ElementoVisualTester

---
+++ pianito/src/teclas/ElementoVisualTester.java
@@ -0,0 +1,25 @@
+package teclas;
+
+import java.awt.*;
+
+public class ElementoVisualTester {
+    private Graphics graphics;
+    private ElementoVisual elemento;
+
+    public ElementoVisualTester(Graphics g, ElementoVisual e) {
+        throw new UnsupportedOperationException("no programado");
+    }
+
+    public ElementoVisualTester(ElementoVisual e) {
+        throw new UnsupportedOperationException("no programado");
+    }
+
+    public void hacerPrueba() {
+        throw new UnsupportedOperationException("no programado");
+    }
+
+    public static void main(String[]  args) {
+        throw new UnsupportedOperationException("no programado");
+    }
+}
+
