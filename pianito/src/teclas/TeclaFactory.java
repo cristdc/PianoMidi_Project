34b77e8ae80fe41501b0111e19292d7d91f38b75
rmornun12 <roberto.m.n2005@gmail.com>
2024-05-10 08:49:02 +0200

    Commit TeclaFactory actualizado

--- pianito/src/teclas/TeclaFactory.java
+++ pianito/src/teclas/TeclaFactory.java
@@ -5,6 +5,27 @@ public class TeclaFactory {
 
     }
     public static Tecla crearTecla(int nota){
-        throw new UnsupportedOperationException();
+        int tipo = nota % 12;
+
+        switch (tipo) {
+            case 1:
+            case 3:
+            case 6:
+            case 8:
+            case 10:
+                return new TeclaNegra(nota);
+            case 0:
+            case 5:
+                return new TeclaBlanca1(nota);
+            case 2:
+            case 7:
+            case 9:
+                return new TeclaBlanca2(nota);
+            case 4:
+            case 11:
+                return new TeclaBlanca3(nota);
+            default:
+                throw new IllegalArgumentException("Número de nota musical inválido: " + nota+ ". Los parámetros de notas musicales son del 1 al 11");
+        }
     }
 }
