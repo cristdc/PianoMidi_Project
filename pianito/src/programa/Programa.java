b43bcf4b16adc4b80ffa723a02b41c8cb8e7bf83
Dani28MR <dmrdaniel2804@gmail.com>
2024-05-15 08:47:23 +0200

    programa y cancion.mid

--- pianito/src/programa/Programa.java
+++ pianito/src/programa/Programa.java
@@ -1,7 +1,21 @@
 package programa;
 
+import bpc.daw.consola.*;
+
 public class Programa {
     public static void main(String[] args) {
-        System.out.println("");
+        Consola c = new Consola();
+        CapaFondo cf = c.getCapaFondo();
+        cf.setFondo(new FondoColorSolido(0,0,70));
+        CapaTexto ct = c.getCapaTexto();
+        ct.print("Dime la ruta del archivo: ");
+        Teclado t = c.getTeclado();
+        String ruta = t.leerCadenaCaracteres();
+        PianoSencillo p = new PianoSencillo(24,108);
+        p.setGraphics(c.getCapaCanvas().getGraphics());
+        p.setPosicion(120,90);
+
+
+        t.leerCaracter();
     }
 }
