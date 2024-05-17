7a5b0386530b109c907be6f6afb59c2721e233a9
Marquittos01 <159147500+Marquittos01@users.noreply.github.com>
2024-05-17 08:41:16 +0200

    Ampliacion Tarea1

--- pianito/src/tests/PianoSencilloTest.java
+++ pianito/src/tests/PianoSencilloTest.java
@@ -4,16 +4,24 @@ import org.junit.Test;
 import programa.Piano;
 import programa.PianoSencillo;
 import teclas.ElementoVisualTester;
+import teclas.TeclaBlanca;
 
 import static org.junit.Assert.*;
 
 public class PianoSencilloTest {
     @Test
     public void Test1(){
-        Piano p = new PianoSencillo(36, 72);
+        PianoSencillo p = new PianoSencillo(36, 72);
 
         ElementoVisualTester e = new ElementoVisualTester(p);
         e.hacerPrueba();
 
+    }
+
+    @Test
+    public void Test2(){
+        PianoSencillo p = new PianoSencillo(36, 48);
+        assertEquals(325, p.getAnchura());
+        assertEquals(100, p.getAltura());
     }
 }
