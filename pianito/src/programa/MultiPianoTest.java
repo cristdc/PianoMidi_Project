27c6be700d887b38531b22213026f96bb32fd97a
rmornun12 <roberto.m.n2005@gmail.com>
2024-05-17 08:50:36 +0200

    Commit Multipiano-testSTUB

---
+++ pianito/src/programa/MultiPianoTest.java
@@ -0,0 +1,13 @@
+package programa;
+
+import org.junit.Test;
+import teclas.ElementoVisualTester;
+
+import static org.junit.Assert.*;
+
+public class MultiPianoTest {
+    @Test
+    public void test(){
+        throw new UnsupportedOperationException();
+    }
+}
