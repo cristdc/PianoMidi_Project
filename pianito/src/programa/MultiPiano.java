ed49a4724ddd2ca4a470e0286a02080890cd258a
christdc <cristinadiazcabello@gmail.com>
2024-05-22 08:57:01 +0200

    piano multi hecho

--- pianito/src/programa/MultiPiano.java
+++ pianito/src/programa/MultiPiano.java
@@ -6,15 +6,15 @@ import teclas.TeclaBlanca;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MultiPiano extends Piano{
+public class MultiPiano extends Piano {
     private List<PianoSencillo> pianos;
 
     public MultiPiano(int ti, int tf) {
         super(ti, tf);
         this.pianos = new ArrayList<>();
 
-        for(int i = 0; i<16; i++){
-            this.pianos.add(new PianoSencillo(ti,tf));
+        for (int i = 0; i < 16; i++) {
+            this.pianos.add(new PianoSencillo(ti, tf));
         }
     }
 
@@ -30,15 +30,16 @@ public class MultiPiano extends Piano{
         int y = this.posicion.y;
         for(int i =0; i<this.pianos.size()/2; i++){
             this.pianos.get(i).setPosicion(x, y);
+            this.pianos.get(i).setGraphics(this.graphics);
+            this.pianos.get(i).dibujar();
             y += (int) (TeclaBlanca.ALTURA*1.2);
-
-            if(i==this.pianos.size()/2){
-                x+= this.pianos.get(i).getAnchura();
-            }
         }
+        x += new PianoSencillo(super.getTeclaInicial(),super.getTeclaFinal()).getAnchura();
 
         for(int i = 8; i<this.pianos.size(); i++){
             this.pianos.get(i).setPosicion(x, y);
+            this.pianos.get(i).setGraphics(this.graphics);
+            this.pianos.get(i).dibujar();
             y += (int) (TeclaBlanca.ALTURA*1.2);
         }
 
